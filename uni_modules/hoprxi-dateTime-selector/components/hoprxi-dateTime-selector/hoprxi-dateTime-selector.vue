cd5321655f36f2c9a671d63113eceb3c2c666115
guan <guantemp@126.com>
2023-06-03 18:06:56 +0800

    202306031806

--- uni_modules/hoprxi-dateTime-selector/components/hoprxi-dateTime-selector/hoprxi-dateTime-selector.vue
+++ uni_modules/hoprxi-dateTime-selector/components/hoprxi-dateTime-selector/hoprxi-dateTime-selector.vue
@@ -2,11 +2,11 @@
 	<view class="date_time" @touchmove.prevent.stop="">
 		<view class="header">
 			<view class="flex flex-sub justify-center align-center">
-				<text class="icon-double-arrow margin-right-sm" style="font-size: 14px;" @tap="previousYear"></text>
+				<text class="icon-double-arrow margin-right" style="font-size: 14px;" @tap="previousYear"></text>
 				<text class="cuIcon-back" @tap="previousMonth"></text>
-				<text class="margin-lr">{{showTitle}}</text>
+				<text class="margin-lr-xl">{{showTitle}}</text>
 				<text class="cuIcon-right" @tap="nextMonth"></text>
-				<text class="icon-double-arrow margin-left-sm" @tap="nextYear"
+				<text class="icon-double-arrow margin-left" @tap="nextYear"
 					style="transform:rotate(180deg);font-size: 14px;"> </text>
 			</view>
 			<view>
@@ -23,34 +23,49 @@
 			<view class="background-month">{{calendar.month+1 < 10 ? "0"+(calendar.month+1) : (calendar.month+1)}}
 			</view>
 			<block v-for="row in 6" :key="row">
-				<view class="flex text-bold padding-lr-xxl response" style="height:45px;font-size: 14px;">
+				<view class="flex text-bold padding-lr-xxl response" style="height:45px;">
 					<block v-for="(day,index) in rows(row)" :key="row + ':' + index">
-						<view class="flex flex-sub align-center flex-direction " :class="{'section': day.status === 'withinTheInterval',
+						<view class="flex flex-sub align-center flex-direction text-df " :class="{'section': day.status === 'withinTheInterval',
 										'start':day.status === 'start',
 										'end':day.status === 'end',
 										'coincide':day.status === 'coincide',
 										'bg-olive radius':day.status ==='current'
-										}" @tap.stop="clickDate">
+										}" @tap.stop="clickDate(day)">
 							<text :class="{'text-grey': day.status === 'prepose'||day.status === 'next'}"
 								class="value">{{day.day}}</text>
-							<text style="font-size: 10px;" :class="{'text-white': day.status === 'start'}"
-								v-if="day.status === 'start'" class="prompt">{{startPrompt}}</text>
-							<text style="font-size: 10px;" :class="{'text-white': day.status === 'end'}"
-								v-else-if="day.status === 'end'" class="prompt">{{endPrompt}}</text>
-							<text style="font-size: 10px;" :class="{'text-white': day.status === 'coincide'}"
+							<text :class="{'text-white text-xs': day.status === 'start'}" v-if="day.status === 'start'"
+								class="prompt">{{startPrompt}}</text>
+							<text :class="{'text-white text-xs': day.status === 'end'}" v-else-if="day.status === 'end'"
+								class="prompt">{{endPrompt}}</text>
+							<text :class="{'text-white text-xs': day.status === 'coincide'}"
 								v-else-if="day.status === 'coincide'"
 								class="prompt">{{startPrompt + "/" + endPrompt}}</text>
-							<text v-else style="font-size: 10px;"
+							<text v-else
 								:class="{'text-grey': day.status === 'prepose'||day.status === 'next','text-red':day.lunar.lunarFestival||day.lunar.festival||day.lunar.Term}"
-								class="prompt">{{day.lunar.lunarFestival||day.lunar.festival||day.lunar.Term||(day.lunar.IDayCn==='初一'?day.lunar.IMonthCn:day.lunar.IDayCn)}}</text>
+								class="prompt text-xs">{{day.lunar.lunarFestival||day.lunar.festival||day.lunar.Term||(day.lunar.IDayCn==='初一'?day.lunar.IMonthCn:day.lunar.IDayCn)}}</text>
 						</view>
 					</block>
 				</view>
 			</block>
 		</view>
-		<view class="response text-center flex padding solid-bottom" style="font-size: 14px;">
-			<text>2023-04-01 11:59:59</text><text class="flex-sub">——</text><text>2023-04-30
-				23:59:59</text>
+		<view class="response text-center flex padding solid-bottom text-df">
+			<text>{{range.start.getFullYear()+'-'+formatNum(range.start.getMonth()+1)+'-'+formatNum(range.start.getDate())}}
+				{{formatNum(range.start.getHours())+':'+formatNum(range.start.getMinutes())+':'+formatNum(range.start.getSeconds())}}</text><text
+				class="flex-sub">——</text><text>{{range.end.getFullYear()+'-'+formatNum(range.end.getMonth()+1)+'-'+formatNum(range.end.getDate())}}
+				{{formatNum(range.end.getHours())+':'+formatNum(range.end.getMinutes())+':'+formatNum(range.end.getSeconds())}}</text>
+		</view>
+		<view class="time" v-if="false">
+			<picker-view class="d-picker-view" :indicator-style="itemHeight" :value="timeVal" @change="timeChange">
+				<picker-view-column>
+					<view class="dateItem" v-for="(item,index) in time.hours" :key="index">{{item}}时</view>
+				</picker-view-column>
+				<picker-view-column>
+					<view class="dateItem" v-for="(item,index) in time.minutes " :key="index">{{item}}分</view>
+				</picker-view-column>
+				<picker-view-column>
+					<view class="dateItem" v-for="(item,index) in time.seconds" :key="index">{{item}}秒</view>
+				</picker-view-column>
+			</picker-view>
 		</view>
 		<view class="flex justify-center margin-tb-sm">
 			<button class="cu-btn lg radius shadow bg-black basis-xl" @tap.stop="confirm">
@@ -82,9 +97,33 @@
 		setup(props, content) {
 			const weeks = ['日', '一', '二', '三', '四', '五', '六'];
 			const current = new Date();
+			const formatNum = (n) => {
+				return (Number(n) < 10 ? '0' + Number(n) : Number(n) + '');
+			};
+			const setLastTime = (date) => {
+				date.setHours(23);
+				date.setMinutes(59);
+				date.setSeconds(59);
+				return date;
+			}
+			const time = computed(() => {
+				let hours = [],
+					minutes = [],
+					seconds = [];
+				for (let i = 0; i <= 23; i++) hours.push(formatNum(i));
+				for (let j = 0; j <= 59; j++) {
+					minutes.push(formatNum(j));
+					seconds.push(formatNum(j));
+				}
+				return {
+					hours,
+					minutes,
+					seconds
+				}
+			});
 			const calendar = reactive({
-				year: 1970,
-				month: 0,
+				year: current.getFullYear(),
+				month: current.getMonth(),
 				days: []
 			});
 			const showTitle = computed(() => {
@@ -111,10 +150,23 @@
 			const rows = computed(() => (row) => {
 				return calendar.days.slice((row - 1) * 7, row * 7);
 			});
+			const timeVal = reactive([]);
+			const timeChange = (e) => {
+				let arr = [...e.detail.value];
+				console.log(arr);
+			}
+			const range = reactive({
+				start: props.start ? new Date(props.start) : current,
+				end: props.end ? setLastTime(new Date(props.end)) : setLastTime(current)
+			});
 			const confirm = () => {
-				emits("confirm", {
-					start: props.start,
-					end: props.end
+				console.log({
+					start: range.start,
+					end: range.end
+				});
+				content.emit("confirm", {
+					start: range.start,
+					end: range.end
 				});
 			};
 			const calculate = (date) => {
@@ -138,7 +190,7 @@
 					months[2] = 29;
 				}
 				calendar.year = year;
-				const month = now.getMonth() + 1; //使用1-12月
+				const month = now.getMonth() + 1; //使用1-12月,用于匹配months对象
 				calendar.month = now.getMonth(); //使用0-11月	
 				calendar.days = [];
 				//let firstDay = new Date(`${year}/${calendar.month}/1 00:00:00`);
@@ -146,8 +198,8 @@
 				const week = firstDay.getDay();
 				if (week != 0) {
 					for (let i = week - 1; i >= 0; i--) {
-						console.log("lunar")
-						console.log(year, month - 1, month - 1 == 0 ? months[12] - i : months[month - 1] - i)
+						//console.log("lunar")
+						//console.log(year, month - 1, month - 1 == 0 ? months[12] - i : months[month - 1] - i)
 						calendar.days.push({
 							day: month - 1 == 0 ? months[12] - i : months[month - 1] - i,
 							status: 'prepose',
@@ -166,11 +218,10 @@
 				for (let i = 1, j = 43 - calendar.days.length; i < j; i++) {
 					calendar.days.push({
 						day: i,
-						status: "next",
+						status: 'next',
 						lunar: lunar.solar2lunar(year, month + 1, i)
 					})
 				}
-				console.log(calendar)
 			};
 			const setStatus = (year, month, day) => {
 				const start = props.start ? new Date(props.start) : null;
@@ -186,14 +237,20 @@
 					.getTime()) return 'withinTheInterval';
 				return 'normal';
 			};
-			const clickDate = () => {
-				console.log("clickdate");
+			const clickDate = (day) => {
+				if (day.status === 'prepose') console.log(new Date(calendar.year, calendar.month - 1, day.day));
+				else if (day.status === 'next') console.log(new Date(calendar.year, calendar.month + 1, day.day));
+				else range.start = new Date(calendar.year + '-' + calendar.month + '-' + day.day)
+				console.log(range);
 			};
 			onBeforeMount(() => {
 				calculate();
 			});
 			return {
 				weeks,
+				time,
+				formatNum,
+				range,
 				calendar,
 				showTitle,
 				previousYear,
@@ -203,6 +260,9 @@
 				toDay,
 				rows,
 				clickDate,
+				timeVal,
+				timeChange,
+				confirm
 			}
 		}
 	}
@@ -227,7 +287,7 @@
 		.weeks {
 			display: flex;
 			justify-content: center;
-			padding: 0 10px 10px 10px;
+			padding: 0 8px 8px 8px;
 			flex-wrap: nowrap;
 			border-bottom: 1px solid #aaaaaa;
 			margin-bottom: 2px;
@@ -235,42 +295,28 @@
 
 		.calendars {
 			display: flex;
-			position: relative;
 			flex-direction: column;
 			align-items: center;
 			border-bottom: 1px solid #aaaaaa;
-			padding: 0 10px;
-
+			padding: 0 6px;
 
 			.background-month {
 				position: absolute;
-				text-align: center;
 				font-size: 220px;
 				font-weight: bold;
 				color: rgba(0, 0, 0, 0.1);
 			}
 
-			.dot {
-				display: inline-block;
-				position: absolute;
-				margin-top: 6px;
-				text-align: center;
-				width: 8px;
-				height: 8px;
-				border-radius: 50%;
-				background: #0081ff;
-			}
-
 			.value {
 				display: flex;
 				position: relative;
-				top: calc(50% - 12px);
+				top: calc(50% - 13.5px);
 			}
 
 			.prompt {
 				display: flex;
 				position: relative;
-				top: calc(50% - 13px);
+				top: calc(50% - 14px);
 			}
 
 			.start {
@@ -294,6 +340,33 @@
 				background: #444444;
 				border-radius: 5px;
 				color: #ffffff;
+			}
+		}
+
+		.time {
+			position: absolute;
+			display: inline-block;
+			vertical-align: middle;
+			margin-left: auto;
+			margin-right: auto;
+			width: 95%;
+			height: 238px;
+			overflow: hidden;
+			background-color: rgba(255, 255, 255, 1);
+			z-index: 3;
+
+			.d-picker-view {
+				height: 100%;
+			}
+
+			.dateItem {
+				text-align: center;
+				width: 100%;
+				height: 44px;
+				line-height: 44px;
+				text-overflow: ellipsis;
+				white-space: nowrap;
+				font-size: 15px;
 			}
 		}
 	}
